ac095abda95ff613044da926c3a98433d38eca22
Minhkun912 <minhnguyenduc506@gmail.com>
2023-01-16 16:02:49 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassComponents from './components/ClassComponents';
+import FromStudent from './components/FromStudent';
+import Control from './components/Control';
+
+function App() {
+  return (
+<div>
+  <Control></Control>
+<ClassComponents></ClassComponents>
+    <FromStudent></FromStudent>
+</div>
+  );
+}
+
+export default App;
